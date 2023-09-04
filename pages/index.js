3dd1b3c235d1fb0fd576cd319b85eb749ffa7339
Zmaznevegor <zmaznevegor@gmail.com>
2023-09-04 20:13:19 +0700

    npx

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Klad Syndicate - Portfolio 2023</title>
+        <meta charset="UTF-8" />
+      </Head>
+      <iframe src="/portfolio.pdf" style={{
+        position: 'fixed', top: 0, left: 0, bottom: 0, right: 0, width: '100%', height: '100%',
+        border: 'none', margin: 0, padding: 0, overflow: 'hidden', zIndex: 999999
+      }}></iframe>
+    </div>
+  )
+}
